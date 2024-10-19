6e2798e09a56d80f8ebaa958fda83c3aead1c1d1
FED-02 <gk2949@gmail.com>
2024-10-19 17:00:04 +0900

    db 연결

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,14 +1,14 @@
 import express from "express";
 import { watch,
-    upload,
-    deleteVideo,
+    getUpload,
+    postUpload,
     getEdit,
     postEdit} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id(\\d+)", watch);
-videoRouter.get("/:id(\\d+)/edit", getEdit);
-videoRouter.get("/:id(\\d+)/edit", postEdit);
+videoRouter.route("/:id(\\d+)").get(watch);
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
